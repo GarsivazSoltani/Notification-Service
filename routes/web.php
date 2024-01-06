f3903ea20c3dc11f9c56310ebfe2593b438ed9c2
GarsivazSoltani <garsi.soltani@gmail.com>
2024-01-06 12:05:04 -0800

    create markdown & UserRegistered mail file & user-registered view file

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Mail\TopicCreated;
+use App\Mail\UserRegistered;
 use Illuminate\Support\Facades\Mail;
 use Illuminate\Support\Facades\Route;
 
@@ -16,6 +17,6 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    Mail::to('garsi.soltani@gmail.com')->send(new TopicCreated);
+    Mail::to('garsi.soltani@gmail.com')->send(new UserRegistered);
     // return view('welcome');
 });
