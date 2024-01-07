f293d3d748e1a8e5a67f793784156d3165a59cdf
GarsivazSoltani <garsi.soltani@gmail.com>
2024-01-07 12:34:26 -0800

    create sms service

--- app/Services/Notification/Notification.php
+++ app/Services/Notification/Notification.php
@@ -2,6 +2,7 @@
 namespace App\Services\Notification;
 
 use App\Models\User;
+use GuzzleHttp\Client;
 use Illuminate\Mail\Mailable;
 use Illuminate\Support\Facades\Mail;
 
@@ -9,5 +10,27 @@ class Notification
 {
     public function sendEmail(User $user, Mailable $mailable) {
         return Mail::to($user)->send($mailable);
+    }
+
+    public function sendSms(User $user, String $text){
+        $client = new Client();
+        // dd($options);
+
+        $response = $client->post(config('services.sms.uri'), $this->prepareDataForSms($user, $text));
+        return $response->getBody();
+    }
+
+    private function prepareDataForSms(User $user, String $text) {
+        $data =
+            array_merge(
+                config('services.sms.auth'),
+                [
+                    'op' => 'send',
+                    'message' => $text,
+                    'to' => [$user->phone_number],
+                ]
+            );
+        
+        return ['json' => $data];
     }
 }
